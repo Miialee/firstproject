803907c27f928a021861e39a28ba1ee6d735787c
Miialee <mrazafintsalama@gmail.com>
2018-12-16 14:30:59 +0300

    suppression de volumes.py

--- volumes.py
+++
@@ -1,8 +0,0 @@
-def volume():
-    volumes=[1, 5, 8, 6, 45, 18, 2, 8, 85]
-    for name in volumes:
-        if name>40:
-            print(str(name) + "is too loud")
-        else:
-            print(str(name) + "is too low")
-volume()
